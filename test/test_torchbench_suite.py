1cce7c1f8ba92658cd7888b6888ea280e9833d2e
Bert Maher <bertrand@fb.com>
2025-07-22 07:38:37 -0700

    Add simple test for torchbench and topn

---
+++ test/test_torchbench_suite.py
@@ -0,0 +1,39 @@
+import pytest
+import torch
+from BackendBench.torchbench_suite import TorchBenchOpTest
+
+
+class TestOpTest:
+    def test_op_test(self):
+        op_test = TorchBenchOpTest(
+            "aten.relu.default", ["((T([32, 128, 512], f16),), {})"], None
+        )
+        for test in op_test.correctness_tests:
+            args, kwargs = test.args, test.kwargs
+            arg, *extras = args
+            assert arg.shape == torch.Size([32, 128, 512])
+            assert arg.dtype == torch.float16
+            assert kwargs == {}
+            assert extras == []
+
+            torch.testing.assert_close(torch.relu(arg), op_test.op(arg))
+
+    def test_topn(self):
+        op_test = TorchBenchOpTest(
+            "aten.relu.default",
+            [
+                "((T([32, 128, 512], f16),), {})",
+                "((T([32, 256, 512], f16),), {})",
+            ],
+            1,
+        )
+        assert len(op_test.tests()) == 1
+        for test in op_test.correctness_tests:
+            args, kwargs = test.args, test.kwargs
+            arg, *extras = args
+            assert arg.shape == torch.Size([32, 256, 512])
+            assert arg.dtype == torch.float16
+            assert kwargs == {}
+            assert extras == []
+
+            torch.testing.assert_close(torch.relu(arg), op_test.op(arg))
