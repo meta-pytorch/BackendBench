b4c9ce50b2d897e05aa2bf21b6e8b5f06334b3a6
Mark Saroufim <marksaroufim@meta.com>
2025-08-15 18:46:16 -0700

    Add License headers

--- BackendBench/scripts/pytorch_operators.py
+++ BackendBench/scripts/pytorch_operators.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
 """PyTorch operator utilities for BackendBench analysis"""
 
 import urllib.request
