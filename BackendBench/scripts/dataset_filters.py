2a50a4aad8d38e1d0151d891d6a47e9299e1ecf8
Sahan Paliskara <sahanp@meta.com>
2025-08-19 09:28:53 -0700

    licenses

--- BackendBench/scripts/dataset_filters.py
+++ BackendBench/scripts/dataset_filters.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
 # Operators to skip for indexing ops that need valid indices
 SKIP_OPERATORS = [
     "embedding",
--- BackendBench/scripts/parquet_trace_converter.py
+++ BackendBench/scripts/parquet_trace_converter.py
@@ -1,3 +1,9 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD 3-Clause license found in the
+# LICENSE file in the root directory of this source tree.
+
 # utility functions to convert parquet and trace files back and forth
 
 import hashlib
