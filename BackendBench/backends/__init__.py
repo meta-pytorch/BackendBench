3f72eea3c81ee7a1173ebe96f660eca386678f0d
Sahan Paliskara <sahanp@meta.com>
2025-08-27 22:05:56 -0700

    restore llm backend

--- BackendBench/backends/__init__.py
+++ BackendBench/backends/__init__.py
@@ -17,6 +17,7 @@ from .base import Backend
 from .directory import DirectoryBackend
 from .flag_gems import FlagGemsBackend
 from .kernel_agent import KernelAgentBackend
+from .llm import LLMBackend
 from .llm_relay import LLMRelayBackend
 
 __all__ = [
@@ -24,6 +25,7 @@ __all__ = [
     "DirectoryBackend",
     "AtenBackend",
     "FlagGemsBackend",
+    "LLMBackend",
     "LLMRelayBackend",
     "KernelAgentBackend",
 ]
