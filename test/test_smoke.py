cbce6c6e5572fe376f8cef7fc4a1d542b1231bec
Jiannan Wang <jiannanwang@meta.com>
2025-09-09 08:00:31 -0700

    remove triton

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -4,7 +4,6 @@
 # This source code is licensed under the BSD 3-Clause license found in the
 # LICENSE file in the root directory of this source tree.
 
-import importlib.util
 
 import pytest
 import torch
@@ -12,10 +11,6 @@ import torch
 import BackendBench.backends as backends
 from BackendBench.eval import eval_one_op
 from BackendBench.suite import SmokeTestSuite
-
-HAS_TRITON = importlib.util.find_spec("triton") is not None
-
-pytestmark = pytest.mark.skipif(not HAS_TRITON, reason="triton not available")
 
 
 class TestSmoke:
